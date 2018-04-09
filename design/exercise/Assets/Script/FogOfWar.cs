ca25de75d2601f402b3969554a4e6a89d180a5dd
astrophelX <1564920673@qq.com>
2018-04-09 20:08:28 +0800

    demo

---
+++ design/exercise/Assets/Script/FogOfWar.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FogOfWar : MonoBehaviour
+{
+    public int FogMaxSize;
+    public GameObject Fog;
+    public float Hight;
+
+
+	void Start ()
+	{
+	    FogMaxSize *=(int)Fog.transform.localScale.x;
+        for (int i = 0; i < FogMaxSize; i+=(int)Fog.transform.localScale.x)
+	    {
+            for (int j = 0; j < FogMaxSize; j+=(int)Fog.transform.localScale.x)
+            {
+                Instantiate(Fog, new Vector3(0 + i, Hight, 0 + j), Quaternion.identity);
+            }
+	    }	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+}
