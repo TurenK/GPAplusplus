b04f1aec02602003d73356fbb889a4bec478c4b4
SundayZhao <664834952@qq.com>
2018-04-13 16:46:10 +0800

    主界面
    主界面

---
+++ code/exercise/Assets/Script/Base.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class Base : MonoBehaviour
+{
+    public float HP=100;
+    private Building building;
+    public GameObject Hps;
+    public bool IsSelect = false;
+    private int laymask;
+    public void TakeDamage(float Damage)
+    {
+        HP -= Damage;
+    }
+
+    void Start ()
+	{
+        laymask = LayerMask.GetMask("Friendlyforce");
+        GameObject child = Instantiate(Hps) as GameObject;
+        child.transform.parent = this.transform;
+	    if (this.gameObject.tag=="Emeny")
+	    {
+	        return;
+	    }
+	    building = GameObject.Find("BuildManager").GetComponent<Building>();
+        building.Builded.Add(this.gameObject);
+	}
+	
+
+	void Update ()
+    {
+        Beselect();
+	    if (HP <= 0)
+	    {
+            Destroy(this.gameObject);
+	    }
+    }
+
+    void Beselect()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, laymask))
+        {
+            if (hit.collider.gameObject.tag == "Friendlyforce")
+            {
+                if (Input.GetMouseButton(0))
+                {
+                    try
+                    {
+                        hit.collider.gameObject.GetComponent<Base>().IsSelect = true;
+                    }
+                    catch 
+                    {
+                    }                 
+                }
+            }
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (this.gameObject.tag=="Emeny"&&other.gameObject.tag=="Bullet")
+        {
+            TakeDamage(other.GetComponent<Bullet>().ATK);
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ code/exercise/Assets/Script/Building.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Building : MonoBehaviour
+{
+    public bool IsBarracks=false;
+    public bool IsSelectBuild=false;
+    public List<GameObject> Builded;
+    private bool IsInstantiate=false;
+    private GameObject BuildingObject;
+    public GameObject Barracks;
+    private int laymask;
+	void Start () 
+    {
+        laymask = LayerMask.GetMask("Terrain");
+	}
+
+    public void OnClickBuildBarracks()
+   {
+       IsInstantiate = true;    
+       BuildingObject = Barracks;
+   }
+
+    public void BarracksInstantiateProduct()
+    {
+        for (int i = 0; i < Builded.Count; i++)
+        {
+            try
+            {
+                if (Builded[i].GetComponent<FriendlyforcesBuild>().BulidTag == "Barracks(Clone)")
+                {
+                    Builded[i].GetComponent<FriendlyforcesBuild>().InstantiateProduct();
+                    break;
+                }             
+            }
+            catch 
+            {               
+                
+            }
+        }       
+    }
+
+    void Update () 
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (IsInstantiate == true)
+        {          
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, laymask))
+            {
+                if (hit.collider.gameObject.tag == "Terrain")
+                {
+                    if (IsSelectBuild == false)
+                    Instantiate(BuildingObject, new Vector3(hit.point.x, Terrain.activeTerrain.SampleHeight(hit.point) + (this.transform.localScale.y / 2), hit.point.z), Quaternion.identity);
+                    IsSelectBuild = true;
+                    IsInstantiate = false;
+                }
+            }        
+        }      
+	}
+}
