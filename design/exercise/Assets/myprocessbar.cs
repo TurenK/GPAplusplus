a82afe58146280f012e67316f6bb27a833c1d3d6
SundayZhao <664834952@qq.com>
2018-04-18 21:00:06 +0800

    加载界面随机取图

--- design/exercise/Assets/myprocessbar.cs
+++ design/exercise/Assets/myprocessbar.cs
@@ -3,26 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class myprocessbar : MonoBehaviour {
+public class myprocessbar : MonoBehaviour
+{
     public Transform myprogress;
-    public Transform setText;
-    public Transform loadingtext;
-    private float currentAmount=0;
-    private float m_speed=20;
+    private float currentAmount = 0;
+    private float m_speed = 20;
 
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update()
+    {
+        myprogress.GetComponent<RectTransform>().sizeDelta = new Vector2(Screen.width/3, Screen.height);
         if (currentAmount < 100)
         {
             currentAmount += m_speed * Time.deltaTime;
-            setText.GetComponent<Text>().text = ((int)currentAmount).ToString() + "%";
-            setText.gameObject.SetActive(true);
+            // setText.GetComponent<Text>().text = ((int)currentAmount).ToString() + "%";
+            //setText.gameObject.SetActive(true);
         }
         else
         {
-            loadingtext.GetComponent<Text>().text = "Done";
-            setText.gameObject.SetActive(false);
+            // loadingtext.GetComponent<Text>().text = "Done";
+            // setText.gameObject.SetActive(false);
         }
         myprogress.GetComponent<Image>().fillAmount = currentAmount / 100;
+    }
+
+
+    void Awake()
+    {
+        myprogress.GetComponent<RectTransform>().sizeDelta = new Vector2(Screen.width/3, Screen.height);
     }
 }
