2fce23c75cc0d7cfa391b2ffdc9106b15910e546
SundayZhao <664834952@qq.com>
2018-04-20 13:16:37 +0800

    合并

---
+++ design/exercise/Assets/Script/Destroy.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destroy : MonoBehaviour {
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Friendlyforce"||other.gameObject.tag=="EmenyBullet")
+        {
+            return;
+        }
+            Destroy(other.gameObject);              
+    }
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
